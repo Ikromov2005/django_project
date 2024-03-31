f27ef8791b797026cef1c1eaa2d2a52b4b24dd5c
Ikromov2005 <159450064+Ikromov2005@users.noreply.github.com>
2024-03-31 21:02:37 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static')),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
